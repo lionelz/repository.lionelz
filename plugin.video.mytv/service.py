b238bc93dabf44480ac471a0180c24537445faa6
Lionel Z <ariel.zerbib@gmail.com>
2017-02-13 22:04:26 +0200

    use 127.0.0.1

--- plugin.video.mytv/service.py
+++ plugin.video.mytv/service.py
@@ -36,21 +36,21 @@ def iptvsimple_settings(iptvsimple_addon, addon_iptvsimple_path):
     setgs = service_parsers.setting_parser(settings_file)
 
     setgs['epgCache'] = 'false'
-    setgs['epgPath'] = 'http://localhost:12345/epg.xml'
+    setgs['epgPath'] = 'http://127.0.0.1:12345/epg.xml'
     setgs['epgPathType'] = '1'
     setgs['epgTSOverride'] = 'false'
     setgs['epgTimeShift'] = '1.000000'
-    setgs['epgUrl'] = 'http://localhost:12345/epg.xml'
+    setgs['epgUrl'] = 'http://127.0.0.1:12345/epg.xml'
 
-    setgs['logoBaseUrl'] = 'http://localhost:12345/logos'
+    setgs['logoBaseUrl'] = 'http://127.0.0.1:12345/logos'
     setgs['logoFromEpg'] = '0'
-    setgs['logoPath'] = 'http://localhost:12345/logos'
+    setgs['logoPath'] = 'http://127.0.0.1:12345/logos'
     setgs['logoPathType'] = '1'
 
     setgs['m3uCache'] = 'false'
-    setgs['m3uPath'] = 'http://localhost:12345/iptv.m3u'
+    setgs['m3uPath'] = 'http://127.0.0.1:12345/iptv.m3u'
     setgs['m3uPathType'] = '1'
-    setgs['m3uUrl'] = 'http://localhost:12345/iptv.m3u'
+    setgs['m3uUrl'] = 'http://127.0.0.1:12345/iptv.m3u'
 
     setgs['startNum'] = '1'
 
